aa51748bf4c507a43222abf4e1825a50c5a12fa4
RaiderMav <raidermaverick@gmail.com>
2017-09-15 20:09:08 -0500

    correct server.js file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 require('./config/config')
 
-const _ = require('lodash'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  { ObjectID } = require('mongodb'),
-  { mongoose } = require('./db/mongoose'),
-  { Todo } = require('./models/todo'),
-  { User } = require('./models/user'),
-  { authenticate } = require('./middleware/authenticate'),
-  app = express(),
-  port = process.env.PORT
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
+
+var {mongoose} = require('./db/mongoose')
+var {Todo} = require('./models/todo')
+var {User} = require('./models/user')
+var {authenticate} = require('./middleware/authenticate')
+
+var app = express()
+const port = process.env.PORT
 
 app.use(bodyParser.json())
 
@@ -30,7 +32,7 @@ app.get('/todos', authenticate, (req, res) => {
   Todo.find({
     _creator: req.user._id
   }).then((todos) => {
-    res.send({ todos })
+    res.send({todos})
   }, (e) => {
     res.status(400).send(e)
   })
@@ -51,7 +53,7 @@ app.get('/todos/:id', authenticate, (req, res) => {
       return res.status(404).send()
     }
 
-    res.send({ todo })
+    res.send({todo})
   }).catch((e) => {
     res.status(400).send()
   })
@@ -65,14 +67,14 @@ app.delete('/todos/:id', authenticate, (req, res) => {
   }
 
   Todo.findOneAndRemove({
-    _creator: req.user._id,
-    _id: id
+    _id: id,
+    _creator: req.user._id
   }).then((todo) => {
     if (!todo) {
       return res.status(404).send()
     }
 
-    res.send({ todo })
+    res.send({todo})
   }).catch((e) => {
     res.status(400).send()
   })
@@ -92,35 +94,20 @@ app.patch('/todos/:id', authenticate, (req, res) => {
     body.completed = false
     body.completedAt = null
   }
-  Todo.findOneAndUpdate({
-    _id: id,
-    _creator: req.user._id
-  }, {
-    $set: body
-  }, {
-    new: true
-  }).then((todo) => {
+
+  Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send()
     }
+
     res.send({todo})
   }).catch((e) => {
-    res.status(400).send
+    res.status(400).send()
   })
 })
-//   Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
-//     if (!todo) {
-//       return res.status(404).send()
-//     }
-
-//     res.send({ todo })
-//   }).catch((e) => {
-//     res.status(400).send()
-//   })
-// })
 
 // POST /users
-app.post(`/users`, (req, res) => {
+app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
   var user = new User(body)
 
@@ -138,11 +125,11 @@ app.get('/users/me', authenticate, (req, res) => {
 })
 
 app.post('/users/login', (req, res) => {
-  body = _.pick(req.body, ['email', 'password'])
+  var body = _.pick(req.body, ['email', 'password'])
 
   User.findByCredentials(body.email, body.password).then((user) => {
     return user.generateAuthToken().then((token) => {
-      res.header(`x-auth`, token).send(user)
+      res.header('x-auth', token).send(user)
     })
   }).catch((e) => {
     res.status(400).send()
@@ -161,4 +148,4 @@ app.listen(port, () => {
   console.log(`Started up at port ${port}`)
 })
 
-module.exports = { app }
+module.exports = {app}
